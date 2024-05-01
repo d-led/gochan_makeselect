b4f0f8a4d5c05ccc61ae33d4a7e9340f55099898
Dmitry Ledentsov <dmlled@yahoo.com>
2024-05-01 07:32:21 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# gochan_makeselect
-runnable examples from https://songlh.github.io/paper/go-study.pdf
+# gochan makeselect
+
+- runnable examples from https://songlh.github.io/paper/go-study.pdf
+- original repository: https://github.com/system-pclub/go-concurrency-bugs
+
+# Original Article
+
+```citation
+@inproceedings{go-study-asplos,
+    author = {Tu, Tengfei and Liu, Xiaoyu and Song, Linhai and Zhang, Yiying},
+    title = {Understanding Real-World Concurrency Bugs in Go},
+    booktitle = {ASPLOS},
+    year = {2019},
+}
+```
---
+++ bad_test.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"sync/atomic"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBad(t *testing.T) {
+	t.Run("Figure 1. A blocking bug caused by channel.", func(t *testing.T) {
+		halfDelay := 250 * time.Millisecond
+		delay := 2 * halfDelay
+
+		fn := func() int { time.Sleep(delay); return 42 }
+		exited := atomic.Bool{}
+
+		ch := make(chan int)
+		go func() {
+			result := fn()
+			ch <- result // block
+			exited.Store(true)
+		}()
+
+		select {
+		case result := <-ch:
+			fmt.Println(result)
+		case <-time.After(halfDelay):
+			fmt.Println("timeout")
+		}
+
+		time.Sleep(delay)
+
+		assert.Truef(t, exited.Load(), "the goroutine is now stuck and leaked")
+	})
+}
